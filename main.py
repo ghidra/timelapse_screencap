eaf6a93e8933385101704693975c6e75303fd4d3
ghidra <ghidra@gmail.com>
2015-01-14 17:50:45 -0500

    scale argument

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from screencap import screencap
 seq=0
 save_path=os.getcwd()
 save_name="screencap"
+scale=1.0
 interval=1
 
 def main(kwargs):
@@ -16,19 +17,23 @@ def main(kwargs):
 	#second is the filename
 	#third is the interval in seconds
 
-	global save_path
 	global save_name
+	global scale
+	global save_path
 	global interval
 
 	if(len(kwargs)>1):
 		if(kwargs[1]!=""):
-			save_path = kwargs[1]
+			save_name = kwargs[1]
 	if(len(kwargs)>2):
-		if(kwargs[2]!=""):
-			save_name = kwargs[2] 
+                if(kwargs[2]!=""):
+                        scale = float(kwargs[2])
 	if(len(kwargs)>3):
-                if(kwargs[3]!=""):
-                        interval = kwargs[3]
+		if(kwargs[3]!=""):
+			save_path = kwargs[3] 
+	if(len(kwargs)>4):
+                if(kwargs[4]!=""):
+                        interval = float(kwargs[4])
 
 
 	#screencap(save_path,save_name+str(seq),1)
@@ -37,11 +42,12 @@ def main(kwargs):
 def capture():
 	global seq
 	global save_path
+	global scale
 	global save_name
 	global interval
 	
 	#print "running:"+str(seq)
-	screencap(save_path,save_name+str(seq))
+	screencap(save_path,save_name+str(seq),scale)
 	seq=seq+1
 	threading.Timer(interval, capture).start()
 
--- screencap.py
+++ screencap.py
@@ -1,7 +1,7 @@
 import gtk.gdk
 import math
 
-def screencap( path, name, e=0 ):
+def screencap( path, name,size_multiplier=1.0, e=0 ):
 	w = gtk.gdk.get_default_root_window()
 	sz = w.get_size()
 	if(e): print "The size of the window is %d x %d" % sz
@@ -9,7 +9,14 @@ def screencap( path, name, e=0 ):
 	pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
 	if (pb != None):
 		save_to = path+"/"+name+".png"
-    		pb.save(save_to,"png")
+		if(size_multiplier<1.0):
+			width = int(math.floor(sz[0]*size_multiplier))
+        		height = int(math.floor(sz[1]*size_multiplier))
+			#npb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,width,height )
+			npb = pb.scale_simple(width,height,gtk.gdk.INTERP_BILINEAR)
+			npb.save(save_to,"png")
+		else:
+    			pb.save(save_to,"png")
     		if(e): print "Screenshot saved to:"+save_to
 	else:
     		if(e): print "Unable to get the screenshot."
